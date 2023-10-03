617421cee0b1c9b37575fa71121944459c2cbda6
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-10-03 14:53:43 +0530

    nav menu complete

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
- 
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".nav__link");
+
+// ~~~~~~~~~~~~~~~~~~~~~ TOGGLE MENU ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~  MENU <SHOW> ~~~~~
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// ~~~~  MENU HIDDEN ~~~~~~
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// ~~~~ REMOVE MENU MOBILE ~~~
+
+navLink.forEach(link=> link.addEventListener("click",()=>{
+    navMenu.classList.remove("show-menu");
+
+}))
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~ SCROLL SECTION ACTIVE LINK    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~ SHOW SCROLL UP  ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~ DARK LIGHT THEME  ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~~~~~~~~~~
