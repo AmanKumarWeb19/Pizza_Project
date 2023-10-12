b4a10679346c09023404e3cbd0af385910f6699f
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-10-12 13:15:28 +0530

    header style

--- main.js
+++ main.js
@@ -31,6 +31,14 @@ navLink.forEach((link) =>
 
 // ~~~~~~~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~~~~~~~~~~
 
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 350
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
 // ~~~~~~~~~~~~~~~~~~~~~ SCROLL SECTION ACTIVE LINK    ~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ~~~~~~~~~~~~~~~~~~~~~ SHOW SCROLL UP  ~~~~~~~~~~~~~~~~~~~~~~~~
